3d45f3e3796ac48016d7e03bd828de8b7341e9ba
myApple <myapple@myapplemac.local>
2016-03-29 20:19:32 +0800

    Initial Commit

---
+++ LOLWallpapers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOLWallpapers
+//
+//  Created by myApple on 16/3/29.
+//  Copyright © 2016年 crane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LOLWallpapers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOLWallpapers
+//
+//  Created by myApple on 16/3/29.
+//  Copyright © 2016年 crane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
